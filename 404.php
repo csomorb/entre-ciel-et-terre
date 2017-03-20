fd81d8770d1664ec24bf8311fd2a890fa7d387ae
csomorb <csomorbarnabas1@gmail.com>
2017-03-20 23:49:31 +0000

    menu langues

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <img class="responsive-img" src="./img/404.JPG">
 <div>
-    <h1 class="title is-1 h404">404 - Ben elle est pas là la page, ça alors...</h1>
+    <h1 class="title is-1 h404">404 - Ben elle est pas là la page, ça alors, try again tomorrow...</h1>
     
 </div>
