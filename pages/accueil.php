b5cb2ccbbe18c20b75155aeb4d29c62be6793a76
csomorb <csomorbarnabas1@gmail.com>
2017-03-23 12:24:06 +0000

    menu

---
+++ pages/accueil.php
@@ -0,0 +1,34 @@
+<div class="tile is-ancestor">
+  <!-- All other tile elemnts -->
+  <div class="tile is-parent is-vertical">
+    <div class="tile is-child box">
+      <p class="title"><a href="/equipe">L'équipe</a></p>
+    </div>
+    <div class="tile is-child box">
+      <p class="title">Nous nous engageaons auprès de</p>
+      <a>Espérance banlieu </a> <br/>
+      <a>Fraternité en Iraq </a> <br/>
+      <a>Soeur de la charité </a>
+    </div>
+    <div class="tile is-child box">
+      <p class="title">Contact</p>
+    </div>
+  </div>
+  
+  <div class="tile is-parent is-vertical">
+    <div class="tile is-child box">
+      <p class="title">Nous aider</p>
+    </div>
+    <div class="tile is-child box">
+      <p class="title">Projet</p>
+    </div>
+    <div class="tile is-child box">
+      <p class="title">Itinéraire</p>
+    </div>
+  </div>
+ <div class="tile is-parent">
+    <div class="tile is-child box">
+      <p class="title">Carnet de voyage</p>
+    </div>
+  </div>
+</div>
